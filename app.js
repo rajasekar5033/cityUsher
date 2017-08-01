bb7e3d0a91c07e6d9d05b327097de26014052052
rajasekar5033 <rajasekar.asahi@gmail.com>
2017-08-01 17:32:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,13 @@
 var express = require('express');
 const http  = require('http');
 const path  = require('path');
-const fs    = require('fs');
 const app = express();
 app.use('/node_modules', express.static(__dirname + '/node_modules/'));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('assets', express.static(path.join(__dirname, 'src/assets')))
 app.get('*', (req,res)=>{
-    if(fs.existsSync(path.join(__dirname, 'dist'))){
+
            res.sendFile(path.join(__dirname, 'dist/index.html'));
-    }
-    else{
-                  res.sendFile(path.join(__dirname, 'server_error.html'));
-    }
-   
-    
 });
 
 const port = process.env.PORT || '2000';
